782bb26a3a4debe8d4bc88e2aab55f7d25ce93ad
Julie of the Wolves <juliana.novic@gmail.com>
2018-08-12 18:38:26 -0400

    almost done now

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,15 +13,28 @@ with open(csvpath, newline='') as csvfile:
     list_changes = []
     count = 0
     profit = 0.00  
+    calc_change = 0
+    calc_average = 0
+    
     for row in csvreader:
         count += 1
         profit += float(row [1])
-            
+        list_changes.append(int(row[1]) - calc_change) 
+        test = int(row [1])
+    for item in list_changes:
+        calc_average += item
+    
+
+        
+    average = calc_average/count    
+    maximum = max(list_changes) 
+    minimum = min(list_changes)
     
     print(count)
     print(profit)
- 
-    
+    print(maximum)
+    print(minimum)
+    print(round(average, 2))
     
     
     
