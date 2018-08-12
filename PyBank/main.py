a15c6d638af4b3ee677fc45e6c55e6b9335016b9
Julie of the Wolves <juliana.novic@gmail.com>
2018-08-11 20:08:49 -0400

    third commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,12 +8,20 @@ with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader, None)
     
-
+    list_changes = []
     count = 0
+    profit = 0.00  
     for row in csvreader:
         count += 1
+        profit += float(row [1])
+            
+    
     print(count)
+    print(profit)
+ 
+    
     
     
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,8 +8,32 @@ with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader, None)
+    vote_count = [0, 0, 0, 0]
+    candidates = ["Khan", "Correy", "Li", "O'Tooley"]
     
     count = 0
     for row in csvreader:
         count += 1
-    print(count)
+        if row[2] == "Khan":
+            vote_count[0] += 1
+        if row[2] == "Correy":
+            vote_count[1] += 1
+        if row[2] == "Li":
+            vote_count[2] += 1
+        if row[2] == "O'Tooley":
+            vote_count[3] += 1
+    
+    vpercent = [round(x/count*100, 2) for x in vote_count]
+    
+    
+    print("Election Results")
+    print("--------------------------------------------")
+    print("Total Votes: " + str(count))
+    print("--------------------------------------------")
+    print(candidates[0] + " " + str( vpercent[0]) + " (" + str(vote_count[0]) + ")")
+    print(candidates[1] + " " + str( vpercent[1]) + " (" + str(vote_count[1]) + ")")
+    print(candidates[2] + " " + str( vpercent[2]) + " (" + str(vote_count[2]) + ")")
+    print(candidates[3] + " " + str( vpercent[3]) + " (" + str(vote_count[3]) + ")")
+    print("---------------------------------------------")
+    print("Winner = Khan")
