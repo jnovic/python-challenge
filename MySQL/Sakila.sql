5652d6a5ce84195d0ad1614f8d01b0ac863da1f7
Julie of the Wolves <juliana.novic@gmail.com>
2018-09-28 13:00:18 -0400

    second commit for sql

--- MySQL/Sakila.sql
+++ MySQL/Sakila.sql
@@ -24,6 +24,45 @@ add description blob;
 alter table actor
 drop description;
 
+-- 4a
+Select last_name, count(last_name) as "count" from actor
+group by last_name;
+
+-- 4b
+Select last_name, count(last_name) as "count" from actor
+group by last_name
+having count(last_name) >1;
+
+-- 4c
+SET SQL_SAFE_UPDATES = 0;
+update actor 
+SET first_name = "HARPO"
+WHERE first_name = "GROUCHO" and last_name = "WILLIAMS";
+
+-- 4d
+update actor
+set first_name = "GROUCHO"
+where first_name = "HARPO";
+
+-- 5a
+SHOW CREATE TABLE address;
+
+-- 6a
+Select staff.first_name, staff.last_name, address.address 
+from staff 
+join address 
+on staff.address_id = address.address_id;
+
+-- 6b
+Select staff.first_name, staff.last_name, sum(payment.amount) as "amount rung"
+from staff
+join payment
+on staff.staff_id = payment.staff_id
+group by staff.staff_id;
+
+
+
+
 
 
 
